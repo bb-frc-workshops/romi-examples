17732313304bf3c4bc59f73c0af74504b91d7483
jpokornyiii <jpokornyiii@gmail.com>
2021-01-22 20:25:33 -0500

    Moved config parameters to Constants.java file

--- simpleArm/src/main/java/frc/robot/Constants.java
+++ simpleArm/src/main/java/frc/robot/Constants.java
@@ -13,7 +13,8 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public final class Button {
+    public final class Joystick {
+        // Button mapping for a Logitech gamepad. 
         public static final int X = 1;
         public static final int A = 2;
         public static final int B = 3;
@@ -22,5 +23,39 @@ public final class Constants {
         public static final int TOPRIGHT = 6;
         public static final int BOTTOMLEFT = 7;
         public static final int BOTTOMRIGHT = 8;
+        public static final int BACK = 9;
+        public static final int START = 10;
+        public static final int LEFT_ANALOG = 11;
+        public static final int RIGHT_ANALOG = 12;
+    }
+
+    public final class Arm {
+        // Port configuration to match physical configuration on
+        // Romi board as well as configuration on http://wpilib.local
+        public static final int LIFT_PORT = 4;
+        public static final int TILT_PORT = 3;
+        public static final int GRIPPER_PORT = 2;
+        public static final int GRIPPER_FEEDBACK_PORT = 0;
+
+        // From https://www.pololu.com/docs/0J76/4
+        // Servo ranges are already restricted in the core Servo
+        // class to 600 - 2400 microseconds. However, the
+        // arm does have the ability to bind if the servos move past 
+        // the recommended ranges. With that, we are further restricting
+        // the range of our arm servos. Adjust these for more range of motion
+        // but do so with caution.
+        // Lift range 1000 - 1900 -> .222 - .722
+        // Tilt range 1200 - 1900 -> .333 - .722
+        // Gripper range 500 - 2400 -> 0 - 1
+        public static final double LIFT_MIN = 0.222;
+        public static final double LIFT_MAX = 0.722;
+        public static final double TILT_MIN = 0.333;
+        public static final double TILT_MAX = 0.722;
+        public static final double GRIPPER_MIN = 0.0;
+        public static final double GRIPPER_MAX = 1.0;
+
+        // Incremental amount of change for each button press
+        // or during the periodic check while holding the button down
+        public static final double SERVO_INCREMENT = 0.005;
     }
 }
--- simpleArm/src/main/java/frc/robot/subsystems/Arm.java
+++ simpleArm/src/main/java/frc/robot/subsystems/Arm.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,19 +13,20 @@ public class Arm extends SubsystemBase {
 
   // The Romi has the left and right motors set to
   // PWM channels 0 and 1 respectively
-  private final Servo m_lift = new Servo(4);
-  private final Servo m_tilt = new Servo(3);
-  private final Servo m_gripper = new Servo(2);
-  private final AnalogInput m_gripperRead = new AnalogInput(0);
+  private final Servo m_lift = new Servo(Constants.Arm.LIFT_PORT);
+  private final Servo m_tilt = new Servo(Constants.Arm.TILT_PORT);
+  private final Servo m_gripper = new Servo(Constants.Arm.GRIPPER_PORT);
+  private final AnalogInput m_gripperRead = new AnalogInput(Constants.Arm.GRIPPER_FEEDBACK_PORT);
   private double m_liftPos;
   private double m_tiltPos;
   private double m_gripperPos;
 
-  /** Creates a new Arm. */
+  // Creates a new Arm subsystem
   public Arm() {
     reset();
   }
 
+  // Reset position to resting state
   public void reset() {
     m_liftPos = 0.5;
     m_tiltPos = 0.5;
@@ -35,35 +37,42 @@ public class Arm extends SubsystemBase {
     m_gripper.set(m_gripperPos);
   }
 
+  /** 
+   * Increment tilt motor position
+   * 
+   * @param delta Amount to change motor position
+   */
   public void incrementTilt(double delta) {
-    /* Spec: https://www.pololu.com/docs/0J76/4
-     * Range should be 1200 (down) - 1900 (up) us 
-     */
-    m_tiltPos = saturateLimit(m_tiltPos + delta, 0, 1);;
+    m_tiltPos = saturateLimit(m_tiltPos + delta, Constants.Arm.TILT_MIN, Constants.Arm.TILT_MAX);
     m_tilt.set(m_tiltPos);
   }
 
+  /**
+   * Increment lift motor position
+   * 
+   * @param delta Amount to change motor position
+   */
   public void incrementLift(double delta) {
-    /* Spec: https://www.pololu.com/docs/0J76/4
-     * Range should be 1000 (raised) - 1900 (lowered) us 
-     */
-    m_liftPos = saturateLimit(m_liftPos + delta, 0, 1); 
+    m_liftPos = saturateLimit(m_liftPos + delta,  Constants.Arm.LIFT_MIN,  Constants.Arm.LIFT_MAX); 
     m_lift.set(m_liftPos);
   }
 
+  /** 
+   * Increment gripper motor position
+   * 
+   * @param delta Amount to change motor position
+   */ 
   public void incrementGripper(double delta) {
-    /* Spec: https://www.pololu.com/docs/0J76/4
-     * Range should be 500 (open) - 2400 (closed) us 
-     * AnalogIn range right now from 440 - 1850.
-    */
-    m_gripperPos = saturateLimit(m_gripperPos + delta, 0, 1); 
+    m_gripperPos = saturateLimit(m_gripperPos + delta,  Constants.Arm.GRIPPER_MIN,  Constants.Arm.GRIPPER_MAX); 
     m_gripper.set(m_gripperPos);
   }
 
+  // Get gripper motor position from feedback signal
   public int getGripperPos() {
     return m_gripperRead.getAverageValue();
   }
 
+  // Limit motor range to avoid moving beyond safe ranges
   public double saturateLimit(double val, double l_limit, double u_limit) {
     double outval = val;
     if(val > u_limit) {
