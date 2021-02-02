da543dc0aae9aae1ce38f9125e7f6169a0a2edec
oliviaod9@gmail.com <t47539b*Z>
2021-02-01 21:00:14 -0500

    Added Two-Step command

--- romi-trajectory-ramsete/src/main/java/frc/robot/RobotContainer.java
+++ romi-trajectory-ramsete/src/main/java/frc/robot/RobotContainer.java
@@ -194,6 +194,66 @@ public class RobotContainer {
   //---------------------------end copy ---------------------------
   //
 
+  // ----------------------- start copy ------------------------------
+  //
+  // Niall+'s custom trajectory
+  private Command generateTwoStepCommand() {
+    var autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(DriveConstants.ksVolts, 
+                                       DriveConstants.kvVoltSecondsPerMeter, 
+                                       DriveConstants.kaVoltSecondsSquaredPerMeter),
+            DriveConstants.kDriveKinematics,
+            10);
+
+    TrajectoryConfig config =
+        new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, 
+                             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            .setKinematics(DriveConstants.kDriveKinematics)
+            .addConstraint(autoVoltageConstraint);
+
+    // This trajectory can be modified to suit your purposes
+    // Note that all coordinates are in meters, and follow NWU conventions.
+    // If you would like to specify coordinates in inches (which might be easier
+    // to deal with for the Romi), you can use the Units.inchesToMeters() method
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(
+            new Translation2d(-1.0, 0),
+            new Translation2d(0.0, 0)            
+        ),
+        new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
+        config);
+
+    RamseteCommand ramseteCommand = new RamseteCommand(
+        exampleTrajectory,
+        m_drivetrain::getPose,
+        new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+        new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter),
+        DriveConstants.kDriveKinematics,
+        m_drivetrain::getWheelSpeeds,
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        m_drivetrain::tankDriveVolts,
+        m_drivetrain);
+
+    m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+
+    // Set up a sequence of commands
+    // First, we want to reset the drivetrain odometry
+    return new InstantCommand(() -> m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose()), m_drivetrain)
+        // next, we run the actual ramsete command
+        .andThen(ramseteCommand)
+
+        // Finally, we make sure that the robot stops
+        .andThen(new InstantCommand(() -> m_drivetrain.tankDriveVolts(0, 0), m_drivetrain));
+  } 
+
+  //
+  //---------------------------end copy ---------------------------
+  //
+
   // change the name of the command from generateZigZagCommand() to your own generate{nameHere}Command()
   //
   //-----------------paste here --------------
@@ -220,6 +280,7 @@ public class RobotContainer {
     // Setup SmartDashboard options
     m_chooser.setDefaultOption("Ramsete Trajectory", generateRamseteCommand());
     m_chooser.addOption("ZigZag Trajectory", generateZigZagCommand());
+    m_chooser.addOption("TwoStep Trajectory", generateTwoStepCommand());
     //
     //----   edit and uncomment this to use your command ----
     //
